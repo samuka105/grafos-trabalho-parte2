82146797d9abea80800b15b8e7adc1a12c04c1a7
ViniciusFGMelo <viniciusferrazgmelo@outlook.com>
2024-09-16 12:11:03 -0300

    teste main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
-#include "include/Graph.hpp"
+#include "./include/Graph.hpp"
+#include <iostream>
 
-
-
-int main(int argc, char* argv[])
-{
-    (void)argc;  // Suprimir aviso de parâmetro não utilizado
-    (void)argv;  // Suprimir aviso de parâmetro não utilizado
+int main() {
+    
+    Graph graph("./instances/ipt1.txt");
+    std::cout << "Número de clusters: " << graph.getClusters() << std::endl;
     return 0;
 }
